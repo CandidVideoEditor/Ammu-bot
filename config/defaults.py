bee7642027cfeba2e66a02b286761ee5d8040182
maharshithefox <maharshithefox@gmail.com>
2025-12-27 13:39:44 +0530

    Create defaults.py

---
+++ config/defaults.py
@@ -0,0 +1,5 @@
+# Non-editable default settings
+DEFAULT_LANGUAGE = "en"          # Default language for replies
+EMOJI_PROBABILITY = 0.7          # Chance to add emoji in replies
+MAX_PURGE_LIMIT = float("inf")   # Max /purge messages
+MAX_AGORA_HISTORY = float("inf") # Max /agora history messages
